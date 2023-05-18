7153e30a37a5f569ff09841bb7dbb468c2a7b978
Ak-Rakib <rkrakibsaimon@gmail.com>
2023-05-18 16:47:37 +0600

    error page set

--- src/Components/Pages/Error/Error.jsx
+++ src/Components/Pages/Error/Error.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
+import { useRouteError } from "react-router-dom";
 
 const Error = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const error = useRouteError();
+  return (
+    <div className="relative flex justify-center">
+      <div className="absolute top-52 text-center">
+        <h1 className="text-8xl font-bold">404!!!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          <i>{error.statusText || error.message}</i>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Error;
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,11 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainComponent from "../Components/MainComponent/MainComponent";
 import Error from "../Components/Pages/Error/Error";
+import Home from "../Components/Pages/Home/Home";
+
 
 export const router = createBrowserRouter([
+    
     {
         path: "/",
         element: <MainComponent />,
-        errorElement: <Error></Error>
+        errorElement: <Error />,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>
+            }
+        ]
     }
 ]);
