e40a25f49def93a4ebe871802577e9d396158c80
Ak-Rakib <rkrakibsaimon@gmail.com>
2023-05-18 18:05:28 +0600

    set basic react context

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,6 +1,10 @@
 import React, { createContext } from "react";
+import getAuth from "firebase/auth"
+import app from "../Firebase/firebase";
+
 
 export const AuthContext = createContext();
+const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
   const authInfo = {};
   return (
