d12211f0efc831ba3332a722c7bc3cbaf87f8f29
Ak-Rakib <rkrakibsaimon@gmail.com>
2023-05-21 17:05:09 +0600

    showing data my Toys section

--- src/Components/Pages/MyToys/MyToys.jsx
+++ src/Components/Pages/MyToys/MyToys.jsx
@@ -1,12 +1,14 @@
 import React, { useContext, useEffect, useState } from "react";
 import ShowingMyToys from "./ShowingMyToys/ShowingMyToys";
+import { AuthContext } from "../../../AuthProvider/AuthProvider";
 
 const MyToys = () => {
-    const  {user} = useContext();
+    const  {user} = useContext(AuthContext);
   const [users, setUsers] = useState([]);
+  console.log(users);
 
   useEffect(() => {
-    fetch(`http://localhost:5000/addCollection?email=${user.email}`)
+    fetch(`http://localhost:5000/addCollection?email=${user?.email}`)
       .then((response) => {
         return response.json();
       })
@@ -16,30 +18,36 @@ const MyToys = () => {
   }, []);
 
   return (
-  <div>
+  <div className="mb-20">
+    <h1 className="text-center mb-10 mt-10">
+        <span className="font-bold text-3xl text-green-300">shopDisney</span>
+        <br />
+        <span className="font-bold text-slate-500">You Added This Toys</span>
+      </h1>
     <div>
     <div className="overflow-x-auto w-full">
         <table className="table w-full">
           {/* head */}
           <thead>
             <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
               <th>Name</th>
-              <th>Job</th>
-              <th>Favorite Color</th>
+              <th>Email</th>
+              <th>Toy Name</th>
+              <th>Quantity</th>
+              <th>Sub-Category</th>
+              <th>Details</th>
+              <th>Rating</th>
+              <th>Price</th>
+              <th>Edit</th>
+              <th>Delete</th>
               <th></th>
             </tr>
           </thead>
           <tbody>
             {
-                users.map(addDollData, index => <ShowingMyToys
-                    key={add}
-                    addDollData = {addDollData}
-                    serialNumber={index + 1}
+                users?.map(data => <ShowingMyToys
+                    key={data._id}
+                    data = {data}
                 ></ShowingMyToys>)
             }
            
--- src/Components/Pages/MyToys/ShowingMyToys/ShowingMyToys.jsx
+++ src/Components/Pages/MyToys/ShowingMyToys/ShowingMyToys.jsx
@@ -1,45 +1,51 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const ShowingMyToys = ({ addDollData, serialNumber }) => {
-
-  const {} = addDollData;
+const ShowingMyToys = ({ data }) => {
+  const {
+    _id,
+    name,
+    email,
+    toyName,
+    price,
+    rating,
+    quantity,
+    category,
+    photoURL,
+    details,
+  } = data;
   return (
-    <div>
-      <tr>
-        <th>
-          <label>
-            <input type="checkbox" className="checkbox" />
-          </label>
-        </th>
-        <td>
-          <div className="flex items-center space-x-3">
-            <div className="avatar">
-              <div className="mask mask-squircle w-12 h-12">
-                <img
-                  src="/tailwind-css-component-profile-2@56w.png"
-                  alt="Avatar Tailwind CSS Component"
-                />
-              </div>
-            </div>
-            <div>
-              <div className="font-bold">Hart Hagerty</div>
-              <div className="text-sm opacity-50">United States</div>
+    <tr>
+      <td>
+        <div className="flex items-center space-x-3">
+          <div className="avatar">
+            <div className="mask mask-squircle w-12 h-12">
+              <img src={photoURL} alt="Avatar Tailwind CSS Component" />
             </div>
           </div>
+          <div>
+            <div className="font-bold">{name}</div>
+          </div>
+        </div>
+      </td>
+      <td>{email}</td>
+      <td>{toyName}</td>
+      <td>{quantity}</td>
+      <td>{category}</td>
+      <td>{details}</td>
+      <td>{rating}</td>
+      <td>{"$" + price}</td>
+      <td>
+        <Link className="btn bg-green-300 border-0">
+          Edit
+        </Link>
         </td>
-        <td>
-          Zemlak, Daniel and Leannon
-          <br />
-          <span className="badge badge-ghost badge-sm">
-            Desktop Support Technician
-          </span>
+      <td>
+        <Link className="btn bg-green-300 border-0">
+          Delete
+        </Link>
         </td>
-        <td>Purple</td>
-        <th>
-          <button className="btn btn-ghost btn-xs">details</button>
-        </th>
-      </tr>
-    </div>
+    </tr>
   );
 };
 
